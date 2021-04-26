9c5f4007a983790f5bb20a1e92b7127f418a05f5
Ghamza-Dev <elgassaihamzam54@gmail.com>
2021-04-26 22:46:04 +0000

    Initial commit

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+  /* ========================================
+   * this file contains all the configuration
+   * needed to start the application
+   * =======================================*/
+
+   define('APPROOT', dirname(dirname(__FILE__)));
