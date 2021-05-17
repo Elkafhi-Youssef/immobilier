c18ab369c7b0876de24b90104d44cdfb5d918e37
Ghamza-dev <elgassaihamzam54@gmail.com>
2021-05-17 19:57:39 +0100

    Remove testing code

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -9,36 +9,5 @@
             $this->loadView('users'.DS.'users_getUsers',[]);
 
         }
-        public function getStudentByDiv(){
-            $this->loadView('users'.DS.'users_students',[]);
-
-        }
-
-        public function login(){
-            $data = [
-                'err' => false,
-            ];
-            
-
-            if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])) {
-                $mail = $_POST['email'];
-                $this->setModelInstance('users');
-
-                if ($this->modelInstance->getUserByEmail($mail) == 1) {
-                    $data = [
-                        'mail' => $mail, 
-                    ];
-                    $this->loadView('users/welcome',$data);
-                }else{
-                    $data = [
-                        'err' => 'Error login',
-                    ];
-                    $this->loadView('users/user.login',$data);
-                }
-
-            }else{
-                $this->loadView('users/user.login',$data);
-            }
-        }
 
     }
